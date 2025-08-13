b9d061352525be3f03ea5b3ada8fd0a673b23ff0
ashutosh229 <akumarjha875@gmail.com>
2025-08-14 00:05:28 +0530

    add the app component

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -1,8 +1,8 @@
 import { NhostClient } from "@nhost/nextjs";
 
 const nhost: NhostClient = new NhostClient({
-  subdomain: process.env.SUBDOMAIN,
-  region: process.env.REGION,
+  subdomain: process.env.NEXT_PUBLIC_SUBDOMAIN,
+  region: process.env.NEXT_PUBLIC_REGION,
 });
 
 export default nhost;
